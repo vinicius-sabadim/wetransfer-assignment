8da17d1ae0b036e864a339acc22b78f1bb73a9eb
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-11-02 12:38:05 -0300

    Doing the animation and the handler of buttons

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { useInterval } from '../../hooks'
 import Spinner from '../Spinner'
 
 import './Card.css'
 
+// Simulating a progress that takes 100ms for every update.
+const DELAY = 100
+
 const Card: React.FC = () => {
+  const [isActive, setIsActive] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+  useInterval(
+    () => {
+      // Simulating an update on the progress.
+      const percentage = Math.floor(Math.random() * 5)
+      const newValue = progress + percentage
+      newValue >= 100 ? setProgress(100) : setProgress(newValue)
+    },
+    isActive ? DELAY : null
+  )
+
+  function startUpload() {
+    setIsActive(true)
+  }
+
+  function stopUpload() {
+    setIsActive(false)
+    setProgress(0)
+  }
+
   return (
     <div className="card__container">
-      <Spinner />
-      <button>Start</button>
+      <Spinner isActive={isActive} progress={progress} />
+      {isActive ? (
+        <button className="card__button" onClick={stopUpload}>
+          Stop
+        </button>
+      ) : (
+        <button className="card__button" onClick={startUpload}>
+          Start
+        </button>
+      )}
     </div>
   )
 }
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,35 +2,38 @@ import React from 'react'
 
 import './Spinner.css'
 
+interface SpinnerInterface {
+  isActive: boolean
+  stroke?: number
+  progress: number
+  size?: number
+}
+
 // Spinner inspired by this post https://css-tricks.com/building-progress-ring-quickly/
-
-const Spinner: React.FC = () => {
-  const stroke = 12
-  const progress = 60
-
-  const size = 160
+const Spinner: React.FC<SpinnerInterface> = ({
+  isActive = false,
+  stroke = 12,
+  progress = 0,
+  size = 160
+}) => {
   const radius = size / 2
-
   const normalizedRadius = radius - stroke * 2
   const circumference = normalizedRadius * 2 * Math.PI
   const strokeDashoffset = circumference - (progress / 100) * circumference
 
   return (
-    <div className="spinner__container">
-      <svg height={radius * 2} width={radius * 2}>
+    <div className={`spinner__container ${isActive ? 'spinner--active' : ''}`}>
+      <svg height={size} width={size} className="spinner__svg">
         <circle
-          stroke="#eee"
-          fill="transparent"
+          className="spinner__barBackground"
           strokeWidth={stroke}
           r={normalizedRadius}
           cx={radius}
           cy={radius}
         />
         <circle
-          stroke="#42a1ff"
-          fill="transparent"
+          className="spinner__bar"
           strokeWidth={stroke}
-          strokeLinecap="round"
           r={normalizedRadius}
           style={{
             strokeDasharray: `${circumference} ${circumference}`,
