97e5e3d7c387277cdee75ed7b8316dda3d851961
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-11-02 13:28:44 -0300

    Updates

---
+++ precache-manifest.1ac847ae27d87748a2e9a802a84e7dc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b434ac745ec627a362699b85eb773c5",
+    "url": "/wetransfer-assignment/index.html"
+  },
+  {
+    "revision": "74af580834e71230702c",
+    "url": "/wetransfer-assignment/static/css/main.655011aa.chunk.css"
+  },
+  {
+    "revision": "db6d698c58f08e79a9dd",
+    "url": "/wetransfer-assignment/static/js/2.f6847e83.chunk.js"
+  },
+  {
+    "revision": "74af580834e71230702c",
+    "url": "/wetransfer-assignment/static/js/main.e44613d7.chunk.js"
+  },
+  {
+    "revision": "437bcee7dbb57c236995",
+    "url": "/wetransfer-assignment/static/js/runtime-main.5b42eade.js"
+  }
+]);
